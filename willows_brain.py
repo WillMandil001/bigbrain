3b8da0348e9a777bd0b8b18404ef6cbd77970f5a
WillMandil001 <willmandil@yahoo.co.uk>
2024-10-06 16:52:48 +0100

    clean up

--- willows_brain.py
+++ willows_brain.py
@@ -9,20 +9,6 @@ from IPython.display import clear_output, display
 
 class Brain:
     def __init__(self, shape):
-        # self.state = torch.randint(0, 10, tuple(shape), dtype=torch.uint8)
-        # self.threshold = torch.randint(0, 10, tuple(shape), dtype=torch.uint8)
-        # self.connections_and_strength = [torch.randint(0, 10, tuple(shape), dtype=torch.uint8),
-        #                                  torch.randint(0, 10, tuple(shape), dtype=torch.uint8),
-        #                                  torch.randint(0, 10, tuple(shape), dtype=torch.uint8),
-        #                                  torch.randint(0, 10, tuple(shape), dtype=torch.uint8)]
-
-        # self.state = torch.zeros(tuple(shape), dtype=torch.float16)
-        # self.threshold = torch.zeros(tuple(shape), dtype=torch.float16)
-        # self.connections_and_strength = [torch.zeros(tuple(shape), dtype=torch.float16),
-        #                                  torch.zeros(tuple(shape), dtype=torch.float16),
-        #                                  torch.zeros(tuple(shape), dtype=torch.float16),
-        #                                  torch.zeros(tuple(shape), dtype=torch.float16)]
-
         self.state = torch.tensor([[0.0, 0.0, 0.0, 0.0, 0.0, 0.8],
                                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.8],
                                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
@@ -50,27 +36,23 @@ class Brain:
                                                   [0.0, 0.0, 0.0, 0.4, 0.0, 0.0],
                                                   [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]], dtype=torch.float16),]
 
-        self.history = []
+        self.history = [self.state, self.state]
 
         self.config = {
             "min_output": 0,
             "max_output": 1.0,
             'history_length': 2,
-            "p_spontaneous": 0.001,
+            "p_spontaneous": 0.0001,
             "learning_rate": 1, # should be an integer
             "normalisation_val": 0.1,
             "hebbian_learning_val": 0.01
         }
 
-        # make sure to add a tensor of ones to the list of tensors
-        self.connections_and_strength.append(torch.ones_like(self.connections_and_strength[0]))
-        stacked_tensors = torch.stack(self.connections_and_strength)
-        self.strength = torch.max(stacked_tensors, dim=0)[0]
-
         # Enable interactive mode
         plt.ion()
-        self.fig, ax = plt.subplots(3,4)
-        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+        self.plot_height = 3
+        self.plot_width = 4
+        self.fig, self.axes = plt.subplots(self.plot_height, self.plot_width, figsize=(15, 7))
 
 
     def step(self, input):
@@ -142,6 +124,10 @@ class Brain:
         new_state[indices] = state[indices] - threshold[indices]
         new_state[new_state != 0] = 1
 
+        # add a small probability of spontaneous firing
+        spontaneous_firing = torch.rand_like(state) < self.config['p_spontaneous']
+        new_state[spontaneous_firing] = 1
+
         state_right = new_state*connections_and_strength[0]
         state_left  = new_state*connections_and_strength[1]
         state_up    = new_state*connections_and_strength[2]
@@ -159,60 +145,68 @@ class Brain:
 
         new_state = state_left_new + state_right_new + state_up_new + state_down_new
 
-        # spontaneous firing
-        if self.config['p_spontaneous'] > 0:
-            spontaneous_firing = torch.rand_like(new_state, dtype=torch.float32) < self.config['p_spontaneous']
-            new_state[spontaneous_firing] += self.strength[spontaneous_firing]
-
-        if len(self.history) < self.config['history_length']:  self.history.append(new_state)
-        else:
-            self.history.pop(0)
-            self.history.append(new_state)
-
         # clip the state to the range of the max output
         new_state = torch.clamp(new_state, self.config['min_output'], self.config['max_output'])
+
+        self.history.pop(0)
+        self.history.append(new_state)
 
         return new_state
 
     def print_state(self, state, time_step):
         # Clear the previous plot
-        self.ax1.cla()
-        self.ax2.cla()
-        self.ax3.cla()
+        for i in range(self.plot_height):
+            for j in range(self.plot_width):
+                self.axes[i, j].cla()
 
+        # state
         state_nump = state.numpy()
-        self.ax1.set_xticks(ticks=range(state_nump.shape[1]), labels=range(state_nump.shape[1]))    # Remove axis ticks
-        self.ax1.set_yticks(ticks=range(state_nump.shape[0]), labels=range(state_nump.shape[0]))
-        self.ax1.imshow(state_nump, cmap='gray', aspect='equal')
-
-        # put the value of each pixel in the image
+        self.axes[0, 0].set_xticks(ticks=range(state_nump.shape[1]), labels=range(state_nump.shape[1]))    # Remove axis ticks
+        self.axes[0, 0].set_yticks(ticks=range(state_nump.shape[0]), labels=range(state_nump.shape[0]))
+        self.axes[0, 0].imshow(state_nump, cmap='gray', aspect='equal', vmin=0, vmax=1)
         for i in range(state_nump.shape[0]): 
-            for j in range(state_nump.shape[1]):  self.ax1.text(j, i, f'{state_nump[i, j]:.1f}', color='red', ha='center', va='center')
-        self.ax1.set_title("State")
+            for j in range(state_nump.shape[1]):  self.axes[0, 0].text(j, i, f'{state_nump[i, j]:.1f}', color='red', ha='center', va='center')
+        self.axes[0, 0].set_title("State")
 
-        # plot the weights
-        state_nump = self.connections_and_strength[0].numpy()
-        self.ax2.set_xticks(ticks=range(state_nump.shape[1]), labels=range(state_nump.shape[1]))    # Remove axis ticks
-        self.ax2.set_yticks(ticks=range(state_nump.shape[0]), labels=range(state_nump.shape[0]))
-        self.ax2.imshow(state_nump, cmap='gray', aspect='equal')
-
-        # put the value of each pixel in the image
+        # previous state
+        state_nump = self.history[-2].numpy()
+        self.axes[0, 1].set_xticks(ticks=range(state_nump.shape[1]), labels=range(state_nump.shape[1]))    # Remove axis ticks
+        self.axes[0, 1].set_yticks(ticks=range(state_nump.shape[0]), labels=range(state_nump.shape[0]))
+        self.axes[0, 1].imshow(state_nump, cmap='gray', aspect='equal', vmin=0, vmax=1)
         for i in range(state_nump.shape[0]): 
-            for j in range(state_nump.shape[1]):  self.ax2.text(j, i, f'{state_nump[i, j]:.1f}', color='red', ha='center', va='center')
-        self.ax2.set_title("Weights")
+            for j in range(state_nump.shape[1]):  self.axes[0, 1].text(j, i, f'{state_nump[i, j]:.1f}', color='red', ha='center', va='center')
+        self.axes[0, 1].set_title("State")
+
 
         # plot the threshold
         state_nump = self.threshold.numpy()
-        self.ax3.set_xticks(ticks=range(state_nump.shape[1]), labels=range(state_nump.shape[1]))    # Remove axis ticks
-        self.ax3.set_yticks(ticks=range(state_nump.shape[0]), labels=range(state_nump.shape[0]))
-        self.ax3.imshow(state_nump, cmap='gray', aspect='equal')
-
-        # put the value of each pixel in the image
+        self.axes[1, 0].set_xticks(ticks=range(state_nump.shape[1]), labels=range(state_nump.shape[1]))    # Remove axis ticks
+        self.axes[1, 0].set_yticks(ticks=range(state_nump.shape[0]), labels=range(state_nump.shape[0]))
+        self.axes[1, 0].imshow(state_nump, cmap='gray', aspect='equal', vmin=0, vmax=1)
         for i in range(state_nump.shape[0]): 
-            for j in range(state_nump.shape[1]):  self.ax3.text(j, i, f'{state_nump[i, j]:.1f}', color='red', ha='center', va='center')
-        self.ax3.set_title("Threshold")
+            for j in range(state_nump.shape[1]):  self.axes[1, 0].text(j, i, f'{state_nump[i, j]:.1f}', color='red', ha='center', va='center')
+        self.axes[1, 0].set_title("Threshold")
+
+
+        # plot the weights
+        for p in range(4):
+            state_nump = self.connections_and_strength[p].numpy()
+            self.axes[2, p].set_xticks(ticks=range(state_nump.shape[1]), labels=range(state_nump.shape[1]))    # Remove axis ticks
+            self.axes[2, p].set_yticks(ticks=range(state_nump.shape[0]), labels=range(state_nump.shape[0]))
+            self.axes[2, p].imshow(state_nump, cmap='gray', aspect='equal', vmin=0, vmax=1)
+            for i in range(state_nump.shape[0]): 
+                for j in range(state_nump.shape[1]):  self.axes[2, p].text(j, i, f'{state_nump[i, j]:.1f}', color='red', ha='center', va='center')
+            self.axes[2, p].set_title("Weights")
+
+        # Hide the unused subplots
+        self.axes[0, 2].axis('off')  # Turn off the 3rd subplot
+        self.axes[0, 3].axis('off')  # Turn off the 3rd subplot
+        self.axes[1, 1].axis('off')  # Turn off the 4th subplot
+        self.axes[1, 2].axis('off')  # Turn off the 4th subplot
+        self.axes[1, 3].axis('off')  # Turn off the 4th subplot
 
         # plt.title(f"time step: {time_step}")
+        plt.tight_layout()
         plt.pause(0.01)
 
 env = gym.make("CartPole-v1", render_mode="human")
@@ -223,6 +217,9 @@ brain = Brain(shape=[4, 5])
 
 for time_step in range(1000):
     env.render()
+
+    # observation is between -1 and 1, set it to between 0 and 1
+    observation = (observation + 1) / 2
 
     action = env.action_space.sample()
     action = brain.step(observation)
